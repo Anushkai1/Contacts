afb176b2dbd1254d2fd5aa2c49f9c192b21f718d
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-09 23:19:04 +0530

    Create ContactAdapter.java

---
+++ app/src/main/java/com/example/contacts/ContactAdapter.java
@@ -0,0 +1,59 @@
+package com.example.contacts;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
+
+    Context ct;
+    int[] imgs;
+    MainActivity.Contact[] contacts;
+
+    public ContactAdapter(Context ct2, MainActivity.Contact[] contacts2, int[] imgs2) {
+        ct = ct2;
+        contacts = contacts2;
+        imgs = imgs2;
+    }
+
+    @NonNull
+    @Override
+    public ContactAdapter.ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(ct);
+        View view = inflater.inflate(R.layout.contact, parent, false);
+        return new ContactViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactAdapter.ContactViewHolder holder, int position) {
+        holder.name.setText(contacts[position].name);
+        holder.phone.setText(contacts[position].phone);
+        holder.dp.setImageResource(imgs[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imgs.length;
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, phone;
+        ImageView dp;
+
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.contact_name);
+            phone = itemView.findViewById(R.id.contact_phone);
+            dp = itemView.findViewById(R.id.contactImage);
+        }
+    }
+}
+
