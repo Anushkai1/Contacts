1b208305491f4bdfa1944d2b07168dd172ca4847
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-10 10:10:44 +0530

    OnClick Event of ViewContact

--- app/src/main/java/com/example/contacts/ViewContact.java
+++ app/src/main/java/com/example/contacts/ViewContact.java
@@ -1,6 +1,7 @@
 package com.example.contacts;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,15 +10,18 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ViewContact extends AppCompatActivity {
 
-    private Button btnBack;
+    private Button btnBack, btnFav, btnEdit, btnDelete;
     private TextView textView;
     private ImageView imageView;
+
+    Drawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +51,46 @@ public class ViewContact extends AppCompatActivity {
             }
         });
 
+        btnFav = findViewById(R.id.btnFav);
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawable = ContextCompat.getDrawable(ViewContact.this, R.drawable.heart2);
+                btnFav.setCompoundDrawablesWithIntrinsicBounds(null, null, null, drawable);
+            }
+        });
+
+        btnDelete = findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back();
+            }
+        });
+
+        btnEdit = findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ViewContact.this, Form.class);
+                intent.putExtra("formTitle", "Edit Contact");
+                textView = findViewById(R.id.nameText);
+                intent.putExtra("input_name", textView.getText().toString());
+                textView = findViewById(R.id.phoneText);
+                intent.putExtra("input_phone", textView.getText().toString());
+
+//                imageView = findViewById(R.id.viewProfilePic);
+//                drawable = imageView.getDrawable();
+//                intent.putExtra("dp",  R.drawable.drawable.get);
+
+                startActivity(intent);
+            }
+        });
+
     }
-    private void back(){
+
+    private void back() {
         Intent intent = new Intent(ViewContact.this, MainActivity.class);
         startActivity(intent);
     }
