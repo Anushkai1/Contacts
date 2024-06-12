eea7aa823dfbebfe86f9969fcbb8dad6335ab06e
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-12 23:51:22 +0530

    Create DataBase Helper for use SQL Lite to Save Contacts

---
+++ app/src/main/java/com/example/contacts/DatabaseHelper.java
@@ -0,0 +1,123 @@
+package com.example.contacts;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "contacts.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "contacts";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_PHONE = "phone";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_IS_FAVORITE = "isFavorite";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_PHONE + " TEXT, " +
+                COLUMN_EMAIL + " TEXT, " +
+                COLUMN_IS_FAVORITE + " INTEGER)";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    // Insert a new contact
+    public void insertContact(ContactNew contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, contact.name);
+        values.put(COLUMN_PHONE, contact.phone);
+        values.put(COLUMN_EMAIL, contact.email);
+        values.put(COLUMN_IS_FAVORITE, "0");
+
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    // Update an existing contact
+    public void updateContact(ContactNew contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, contact.name);
+        values.put(COLUMN_PHONE, contact.phone);
+        values.put(COLUMN_EMAIL, contact.email);
+
+        db.update(TABLE_NAME, values, COLUMN_ID + " = ?", new String[]{String.valueOf(contact.id)});
+        db.close();
+    }
+
+    // Delete a contact
+    public void deleteContact(int contactId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, COLUMN_ID + " = ?", new String[]{String.valueOf(contactId)});
+        db.close();
+    }
+
+    // Fetch all contacts
+    public ArrayList<ContactNew> getAllContacts() {
+        ArrayList<ContactNew> contactList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range") ContactNew contact = new ContactNew(
+
+                        cursor.getString(cursor.getColumnIndex(COLUMN_ID)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_NAME)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_PHONE)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_IS_FAVORITE))
+                );
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return contactList;
+    }
+
+    // Search contacts by name
+    public ArrayList<ContactNew> searchContacts(String name) {
+        ArrayList<ContactNew> contactList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_NAME + " LIKE ?", new String[]{"%" + name + "%"});
+
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range") ContactNew contact = new ContactNew(
+                        cursor.getString(cursor.getColumnIndex(COLUMN_ID)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_NAME)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_PHONE)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)),
+                        cursor.getString(cursor.getColumnIndex(COLUMN_IS_FAVORITE))
+                );
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return contactList;
+    }
+}
+
