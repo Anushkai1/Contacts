bf9db54a4208dd4acbdc2e6138670db01af5f0b4
Anushka Isuru <anushka8.ai@gmail.com>
2024-06-14 08:40:39 +0530

    Modify RecylerviewAdapter to implement Fav contacts
    All contacts and Fav
    contacts both contact adapt by this java class

--- app/src/main/java/com/example/contacts/ContactAdapter.java
+++ app/src/main/java/com/example/contacts/ContactAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -15,15 +16,17 @@ import java.util.ArrayList;
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
 
     Context ct;
-    int[] imgs = {R.drawable.e, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j, R.drawable.l};;
+    int type;
+    int[] imgs = {R.drawable.e, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j, R.drawable.l};
+    ;
 
     ArrayList<ContactNew> contacts;
     private SelectListenr selectListenr;
 
-    public ContactAdapter(Context ct2, ArrayList<ContactNew> contacts2, SelectListenr selectListenr2) {
+    public ContactAdapter(Context ct2, ArrayList<ContactNew> contacts2, SelectListenr selectListenr2, int type) {
         ct = ct2;
         contacts = contacts2;
-
+        this.type = type;
         selectListenr = selectListenr2;
     }
 
@@ -31,7 +34,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
     @Override
     public ContactAdapter.ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(ct);
-        View view = inflater.inflate(R.layout.contact, parent, false);
+        View view;
+        if (type == 1) {
+            view = inflater.inflate(R.layout.contact, parent, false);
+        } else {
+            view = inflater.inflate(R.layout.fav_contact, parent, false);
+        }
+
         return new ContactViewHolder(view, selectListenr);
     }
 
@@ -54,6 +63,7 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
 
         TextView id, name, phone, email, isfavorite;
         ImageView dp;
+        ConstraintLayout contactCard;
 
         public ContactViewHolder(@NonNull View itemView, SelectListenr selectListenr2) {
             super(itemView);
@@ -64,7 +74,8 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
             id = itemView.findViewById(R.id.contact_id);
             email = itemView.findViewById(R.id.contact_email);
             isfavorite = itemView.findViewById(R.id.contact_isFavorite);
-
+            isfavorite = itemView.findViewById(R.id.contact_isFavorite);
+            contactCard = itemView.findViewById(R.id.contactCard);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
